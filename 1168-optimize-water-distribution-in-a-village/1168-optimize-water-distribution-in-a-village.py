62de0bc23d84e2873ac98df13f0e037d89c8fee1
julianngong <62028472+julianngong@users.noreply.github.com>
2022-06-26 01:56:59 +0100

    Update 1168-optimize-water-distribution-in-a-village.py

--- 1168-optimize-water-distribution-in-a-village/1168-optimize-water-distribution-in-a-village.py
+++ 1168-optimize-water-distribution-in-a-village/1168-optimize-water-distribution-in-a-village.py
@@ -1,69 +1,38 @@
 class UnionFind:
-    """
-        Implementation of UnionFind without load-balancing.
-    """
-    def __init__(self, size) -> None:
-        """
-        container to hold the group id for each member
-        Note: the index of member starts from 1,
-            thus we add one more element to the container.
-        """
-        self.group = [i for i in range(size + 1)]
-        # the rank of each node for later rebalancing
-        self.rank = [0] * (size + 1)
-
-    def find(self, person: int) -> int:
-        """
-            return the group id that the person belongs to
-        """
-        if self.group[person] != person:
-            self.group[person] = self.find(self.group[person])
-        return self.group[person]
-
-    def union(self, person_1: int, person_2: int) -> bool:
-        """
-            Join the groups together.
-            return:
-                false when the two persons belong to the same group already,
-                otherwise true
-        """
-        group_1 = self.find(person_1)
-        group_2 = self.find(person_2)
-        if group_1 == group_2:
-            return False
-
-        # attach the group of lower rank to the group with higher rank
-        if self.rank[group_1] > self.rank[group_2]:
-            self.group[group_2] = group_1
-        elif self.rank[group_1] < self.rank[group_2]:
-            self.group[group_1] = group_2
+    def __init__(self, nodes):
+        self.roots = []
+        for i in range(nodes):
+            self.roots.append(i)
+            
+    def findroot(self, x):
+        return(self.roots[x])
+    
+    def getroots(self):
+        return(self.roots)
+    
+    def union(self,x,y):
+        if self.roots.count(x)<=self.roots.count(y):
+            self.roots = [self.roots[y] if elem==self.roots[x] else elem for elem in self.roots]
         else:
-            self.group[group_1] = group_2
-            self.rank[group_2] += 1
-
-        return True
-
+            self.roots = [self.roots[x] if elem==self.roots[y] else elem for elem in self.roots]
+            
+    def connecteds(self):
+        uniques = set(self.roots)
+        return(len(uniques))
 
 class Solution:
     def minCostToSupplyWater(self, n: int, wells: List[int], pipes: List[List[int]]) -> int:
-        ordered_edges = []
-        # add the virtual vertex (index with 0) along with the new edges.
-        for index, weight in enumerate(wells):
-            ordered_edges.append((weight, 0, index+1))
-
-        # add the existing edges
-        for house_1, house_2, weight in pipes:
-            ordered_edges.append((weight, house_1, house_2))
-
-        # sort the entire edges by their weights
-        ordered_edges.sort(key=lambda x: x[0])
-
-        # iterate through the ordered edges
-        uf = UnionFind(n)
-        total_cost = 0
-        for cost, house_1, house_2 in ordered_edges:
-            # determine if we should add the new edge to the final MST
-            if uf.union(house_1, house_2):
-                total_cost += cost
-
-        return total_cost
+        edges = list(pipes)
+        edges = edges + [[ind+1,0,well] for ind, well in enumerate(wells)] #the wells list acts as a single extra node 0 which connects to each node and has the weight of having a well
+        edges = sorted(edges, key=lambda x:x[2]) #sort the list based on the 3rd element
+        unionfind = UnionFind(int(n+1)) #make union find because we need to determine if their exists a loop which will happen if we try to union together two elements in the same group
+        numofedges =0
+        total = 0
+        for i in range(len(edges)):
+            x,y,cost = edges[i]
+            if unionfind.findroot(x) != unionfind.findroot(y): # go through the orderered set of edges and join together if edges are not in the same component
+                unionfind.union(x,y)
+                numofedges+=1
+                total+=cost #sum the weight
+                if numofedges == n:
+                    return(total)   
