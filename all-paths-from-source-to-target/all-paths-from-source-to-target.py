0c3388799be9284733d4f9305b0872024ff9e547
julianngong <62028472+julianngong@users.noreply.github.com>
2022-07-01 17:39:35 +0100

    Time: 199 ms, Memory: 17.3 MB - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -2,18 +2,18 @@ class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         n = len(graph)
         listzero = [0]
-        queue = collections.deque()
-        queue.append(listzero)
-        seen = set(tuple([0]))
-        paths = []
-        while queue:
-            currentpath = queue.popleft()
-            currentnode = currentpath[-1]
-            if currentnode == n-1:
+        queue = collections.deque() #make an empty queue
+        queue.append(listzero) #add the list of 0 to the queue which represents the current paths
+        seen = set(tuple([0])) #add each current path to the seen set not just the node
+        paths = [] 
+        while queue: #while the stack is not empty, not although using queue data type we only take elements from the front and add to the back so its a stack
+            currentpath = queue.popleft() #get the bottom element of the stack and remove from stack
+            currentnode = currentpath[-1] #get the most recently reached node from the most current path
+            if currentnode == n-1: #if the most recently reached node is the wanted node then add this path to the list of valid paths
                 paths.append(currentpath)
-            for neighbours in graph[currentnode]:
-                if neighbours not in seen:
-                    newpath = currentpath+[neighbours]
-                    seen.add(tuple(newpath))
-                    queue.append(newpath)
+            for neighbours in graph[currentnode]: #for the most recently reached node check all its neighbours.
+                newpath = currentpath+[neighbours] #add each to the current path to make a new path
+                if tuple(newpath) not in seen: #now want to check if we have checked this path yet or not.
+                    seen.add(tuple(newpath))#if not then add to the list of seen paths
+                    queue.append(newpath)#add this path to the queue of the paths to check further
         return(paths)
