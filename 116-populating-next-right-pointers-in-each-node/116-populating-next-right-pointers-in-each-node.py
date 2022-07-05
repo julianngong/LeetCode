efbb48a1f0fd416f5f38ecb0ad5f1231d96642ab
julianngong <62028472+julianngong@users.noreply.github.com>
2022-07-05 22:22:29 +0100

    Time: 102 ms (41.47%), Space: 15.6 MB (74.99%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -10,20 +10,20 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if root == None:
+        if root == None: #if the tree is empty then just return the tree
             return root
-        queue =  [root]
-        while queue:
-            elemsinrow = len(queue)
-            for i in range(elemsinrow):
-                node = queue.pop(0)
-                if i<elemsinrow-1:
-                    node.next = queue[0]
+        queue =  [root] #make the first element of the queue the root
+        while queue: # while the queue is not empty
+            elemsinrow = len(queue) # note each bfs loop we will add the number of children to the queue so the current length of the queue during the iteration is the number of elements in the row
+            for i in range(elemsinrow):  # for all the nodes in the row
+                node = queue.pop(0) #get the first element of the queue
+                if i<elemsinrow-1: # if its not the last element of the row 
+                    node.next = queue[0]#connect it to the next element coming up in the queue
                 else:
-                    node.next = None
-                if (node.left != None) and (node.right!= None):
-                    queue.append(node.left)
-                    queue.append(node.right)
+                    node.next = None #else assign to none
+                if (node.left != None) and (node.right!= None): # if its not a leaf of the graph
+                    queue.append(node.left) # add the left node to the queue
+                    queue.append(node.right) # add the right node to the queue
         return(root)
             
         
