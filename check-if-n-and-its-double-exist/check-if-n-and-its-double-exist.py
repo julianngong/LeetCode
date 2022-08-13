4a5e00097f2025c52db2568c7cd4bd21c326d1c7
julianngong <62028472+julianngong@users.noreply.github.com>
2022-08-13 18:58:02 +0100

    Time: 88 ms, Memory: 14 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 class Solution:
-    def checkIfExist(self, arr: List[int]) -> bool:
+    def checkIfExist1(self, arr: List[int]) -> bool:
         table = defaultdict()
         for num in arr:
             if num == 0 and arr.count(0)>1:
@@ -10,3 +10,13 @@ class Solution:
                 if ((2*num) in table) or ((num/2) in table):
                         return(True)
         return(False)
+    def checkIfExist(self, arr: List[int]) -> bool:
+        seen = set()
+        for n in arr:
+            if 2*n in seen or n%2==0 and n//2 in seen: 
+                return True
+            else: 
+                seen.add(n)
+        return False
+    
+    
