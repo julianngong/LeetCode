ba25527df45bc0316cfc694bc190c318ee78b48a
julianngong <62028472+julianngong@users.noreply.github.com>
2022-08-13 18:52:11 +0100

    Time: 111 ms, Memory: 13.9 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -6,14 +6,8 @@ class Solution:
         for num in arr:
             if num == 0 and arr.count(0)>1:
                 return(True)
-            if num not in table.keys():
+            elif num != 0 and num not in table.keys():
                 table[num]=index
                 if ((2*num) in table) or ((num/2) in table):
-                    if num == 0:
-                        if zeroseen==True:
-                            return(True)
-                        else:
-                            zeroseen=True
-                    else:
                         return(True)
         return(False)
