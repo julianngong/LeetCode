c12412919d78884e461bdec83d9e5753ab6d49aa
julianngong <62028472+julianngong@users.noreply.github.com>
2022-08-15 23:19:02 +0100

    Time: 625 ms (30.97%), Space: 21.9 MB (88.80%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -6,7 +6,7 @@ class Solution:
                 notin.add(i+1)
         return(list(notin))
     #note dont ever wanna check if element is in list as this is too slow, if you need to do this use a hashmap
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    def findDisappearedNumbers2(self, nums: List[int]) -> List[int]:
         hash_table = {}
         for num in nums:
             hash_table[num]=1
@@ -15,3 +15,14 @@ class Solution:
             if (i+1) not in hash_table:
                 result.append(i+1)
         return(result)
+    def findDisappearedNumbers(self, nums): #we can inplace mark a element of string as seen by multiplying by -1 but when inspecting elements, only look at the absolute value
+        for i in range(len(nums)):
+            indexmatchingseennum = abs(nums[i]) - 1
+            if nums[indexmatchingseennum] > 0:
+                nums[indexmatchingseennum] *= -1
+        
+        result = []    
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] > 0:
+                result.append(i)     
+        return result   
