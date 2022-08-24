d377fa1bd78886a41ab47307c22e13a504edef29
julianngong <62028472+julianngong@users.noreply.github.com>
2022-08-25 00:22:01 +0100

    Time: 37 ms (84.97%), Space: 13.8 MB (54.22%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,8 +1,11 @@
-seen={0:0,1:1}
+seen={0:0}
 class Solution:
     def fib(self, n: int) -> int:
         if n in seen:
             return(seen[n])
         else:
-            seen[n]=self.fib(n-1)+self.fib(n-2)
-            return(seen[n])
+            if n ==1 or n==2:
+                seen[n]=1
+            else:
+                seen[n]=self.fib(n-1)+self.fib(n-2)
+        return(seen[n])
