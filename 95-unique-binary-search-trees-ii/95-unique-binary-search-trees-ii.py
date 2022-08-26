b8a3bf013030dc5c656c022e69b4b1044cea0f8a
julianngong <62028472+julianngong@users.noreply.github.com>
2022-08-26 23:46:35 +0100

    Time: 115 ms (15.62%), Space: 15.7 MB (42.24%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.py
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.py
@@ -5,31 +5,32 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def generateTrees(self, n):
-        """
-        :type n: int
-        :rtype: List[TreeNode]
-        """
-        def generate_trees(start, end):
-            if start > end:
-                return [None,]
-            
-            all_trees = []
-            for i in range(start, end + 1):  # pick up a root
-                # all possible left subtrees if i is choosen to be a root
-                left_trees = generate_trees(start, i - 1)
-                
-                # all possible right subtrees if i is choosen to be a root
-                right_trees = generate_trees(i + 1, end)
-                
-                # connect left and right subtrees to the root i
-                for l in left_trees:
-                    for r in right_trees:
-                        current_tree = TreeNode(i)
-                        current_tree.left = l
-                        current_tree.right = r
-                        all_trees.append(current_tree)
-            
-            return all_trees
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        if n == 0:
+            return []
+        return self.helper(1, n)
+
+
+    def helper(self, start, end):
+        if start > end: # edge case, see exposition below
+            return [None] 
         
-        return generate_trees(1, n) if n else []
+        all_trees = [] # list of all unique BSTs
+        for curRootVal in range(start, end+1): # generate all roots using list [start, end]
+			# recursively get list of subtrees less than curRoot (a BST must have left subtrees less than the root)
+            all_left_subtrees = self.helper(start, curRootVal-1)
+			
+			# recursively get list of subtrees greater than curRoot (a BST must have right subtrees greater than the root)
+            all_right_subtrees = self.helper(curRootVal+1, end) 
+			
+            for left_subtree in all_left_subtrees:   # get each possible left subtree
+                for right_subtree in all_right_subtrees: # get each possible right subtree
+                    # create root node with each combination of left and right subtrees
+                    curRoot = TreeNode(curRootVal) 
+                    curRoot.left = left_subtree
+                    curRoot.right = right_subtree
+					
+					# curRoot is now the root of a BST
+                    all_trees.append(curRoot)
+		
+        return all_trees
