a4778c2a98be8522b62c0ccbf21a961ef9bda7bf
julianngong <62028472+julianngong@users.noreply.github.com>
2022-08-27 13:46:18 +0100

    Time: 120 ms (83.60%), Space: 18.7 MB (71.84%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -4,6 +4,8 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         if k==len(nums):
             return nums
+        #this counter function gives key values pairs of the number followed by its count
         count =Counter(nums)
+        #note the last input is a function which is the values use which will be a function called on each of the values
         return(heapq.nlargest(k,count.keys(),key=count.get))
            
